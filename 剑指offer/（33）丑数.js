3da4672fae660ae6c7c948c04d26f2506edb43d7
wuguanglin <769835910@qq.com>
2018-08-31 19:08:06 +0800

    update (33)

--- 剑指offer/（33）丑数.js
+++ 剑指offer/（33）丑数.js
@@ -4,7 +4,7 @@ function GetUglyNumber_Solution(index){
     res[0]=1;
     let t2=0,t3=0,t5=0;
     for(let i=1;i<index;i++){
-        res[i]=Math.min(...[res[t2]*2,res[t3]*3,res[t5]*5]);
+        res[i]=Math.min(res[t2]*2,ress[t3]*3,res[t5]*5);
         if(res[i]==res[t2]*2) t2++;
         if(res[i]==res[t3]*3) t3++;
         if(res[i]==res[t5]*5) t5++;
