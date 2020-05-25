bb4cb0e8e7290a98e265f36193b5c165fa48ac6c
ericwu <14glwu@gmail.com>
2020-05-26 01:39:15 +0800

    [style] 优化代码

--- 常见JS方法实现/深拷贝deepClone.js
+++ 常见JS方法实现/深拷贝deepClone.js
@@ -1,39 +1,53 @@
 /* eslint no-new-func: "off",no-prototype-builtins: "off" */
-// 可以拷贝函数、Date、RegExp和传统对象
+// 可以拷贝普通函数、Date、RegExp和传统对象
 // 没有拷贝原型、没有解决循环引用
 function deepClone(obj) {
-  if (Object.prototype.toString.call(obj) === '[object Function]') {
+  if (typeof obj === 'function') {
     const str = obj.toString();
-    /^function\s*\w*\s*\(\s*\)\s*\{(.*)/.test(str);
-    const str1 = RegExp.$1.slice(0, -1);
-    return new Function(str1);
+    /^function\s*\w*\s*\((.*)\)\s*\{([\s\S]*)/.exec(str); // 匹配函数体
+    const args = RegExp.$1.split(',').map((item) => item.trim());
+    const str1 = RegExp.$2.slice(0, -1); // 去除末尾花括号
+    return new Function(...args, str1);
   }
   if (!obj || typeof obj !== 'object') return obj;
-  if (Object.prototype.toString.call(obj) === '[object Date]') return new Date(obj);
-  if (Object.prototype.toString.call(obj) === '[object RegExp]') return new RegExp(obj);
+  if (Object.prototype.toString.call(obj) === '[object Date]')
+    return new Date(obj); /*  */
+  if (Object.prototype.toString.call(obj) === '[object RegExp]')
+    return new RegExp(obj);
   const cloneObj = Array.isArray(obj) ? [] : {};
   for (const i in obj) {
     if (obj.hasOwnProperty(i)) {
       // 保证只遍历实例属性
-      cloneObj[i] = typeof obj[i] === 'object' ? deepClone(obj[i]) : obj[i];
+      cloneObj[i] = deepClone(obj[i]);
     }
   }
   return cloneObj;
 }
+const obj = {
+  name: 'xm',
+  birth: new Date(),
+  desc: null,
+  reg: /^123$/,
+  ss: [1, 2, 3],
+  fn: function test(num1, num2) {
+    console.log(num1, num2);
+  }
+};
 
+const obj2 = deepClone(obj);
+console.log(obj, obj2);
+obj.fn(1, 2);
+obj2.fn(1, 2);
 // 如果你所需拷贝的对象含有内置类型并且不包含函数，可以使用 MessageChannel
 
 // 普通的深拷贝可以使用
-JSON.parse(JSON.stringify(object));
+// const obj3 = JSON.parse(JSON.stringify(obj));
+// console.log(obj3);
 // 但是该方法有局限
 // 1.会忽略 undefined
-// 2.不能序列化函数
+// 2.不能序列化函数、复杂对象
 // 3.不能解决循环引用的对象
 
 // 浅拷贝，可以通过Object.assign()来解决
-const a = {
-  age: 1
-};
-const b = Object.assign({}, a);
-a.age = 2;
-console.log(b.age); // 1
+// const obj4 = Object.assign({}, obj);
+// console.log(obj4);
--- 算法/排序方法/quickSort.js
+++ 算法/排序方法/quickSort.js
@@ -9,21 +9,21 @@ function quickSort(a, left, right) {
   }
 }
 function partition(a, left, right) {
-  const key = a[left]; // 一开始让key为第一个数
+  const value = a[left]; // 一开始让key为第一个数
   while (left < right) {
     // 扫描一遍
-    while (key <= a[right] && left < right) {
-      // 如果key小于a[right]，则right递减，继续比较
+    while (value <= a[right] && left < right) {
+      // 如果value小于a[right]，则right递减，继续比较
       right--;
     }
     [a[left], a[right]] = [a[right], a[left]]; // 交换
-    while (key >= a[left] && left < right) {
-      // 如果key大于a[left]，则left递增，继续比较
+    while (value >= a[left] && left < right) {
+      // 如果value大于a[left]，则left递增，继续比较
       left++;
     }
     [a[left], a[right]] = [a[right], a[left]]; // 交换
   }
-  return left; // 把key现在所在的下标返回
+  return left; // 把value现在所在的下标返回
 }
 const arr = [2, 3, 1, 4];
 quickSort(arr, 0, 3);
